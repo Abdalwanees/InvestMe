1cc11e1f1434f8a8a2a204694978181b823b5883
Abdalwanees <gabdo7682@gmail.com>
2024-07-25 14:54:09 +0300

    NavBar Setup

--- src/app/Components/nav-bar-auth/nav-bar-auth.component.ts
+++ src/app/Components/nav-bar-auth/nav-bar-auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './nav-bar-auth.component.html',
   styleUrls: ['./nav-bar-auth.component.scss']
 })
